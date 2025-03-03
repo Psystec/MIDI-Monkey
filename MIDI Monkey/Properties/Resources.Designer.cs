025535cd0d12e58b14bea9ce9e92cb8b31a0a3b0
Psystec <Psystec@Psystec-PC>
2025-03-04 01:24:27 +0200

    Removed unessasary resources and added Default MidiKeyMaps

--- MIDI Monkey/Properties/Resources.Designer.cs
+++ MIDI Monkey/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace MIDI_Monkey.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap CloseButton {
-            get {
-                object obj = ResourceManager.GetObject("CloseButton", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
